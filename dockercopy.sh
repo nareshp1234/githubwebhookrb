a95a3c58fc97168ecd474962bc96444b8df8744f
Naresh Purushotham <46194860+nareshp1234@users.noreply.github.com>
2025-05-02 20:15:36 +0530

    Update dockercopy.sh

--- dockercopy.sh
+++ dockercopy.sh
@@ -2,30 +2,24 @@
 # Check for the correct number of arguments
 
 if [ "$#" -ne 6 ]; then
-    echo "Usage: $0 <jfrog_url> <access_token> <source_repo> <target_repo_name> <image_name> <tag>"
+    echo "Usage: $0 <jfrog_url> <access_token> <releasebundle_name> <releasebundle_version> <environment>"
     exit 1
 fi
 
 # Assign parameters to variables for clarity
 JFROG_URL="${1:?please enter JPD URL. ex - https://workday.jfrog.io}"
 ACCESS_TOKEN="${2:?please provide the identity token}"
-SOURCE_REPO="${3:?please provide the source repository}"
-TARGET_REPO="${4:?please provide the target repository}"
-IMAGE_NAME="${5:?please provide the image name}"
-TAG="$6"
-
-# Check if user has passed tag for docker image, otherwise set it to latest
-if [ -z "${TAG}" ]; then
-    echo "TAG is set to the empty string. using latest tag"
-    TAG="latest"
-fi
+RELEASEBUNDLE_NAME="${3:?please provide the release bundle name}"
+RELEASEBUNDLE_VERSION="${4:?please provide the release bundle version}"
+ENVIRONMENT="${5:?please provide the release bundle environment}"
 
 # Rest API to copy artifact from one repo to another
-DOCKER_COPY_API_URL="$JFROG_URL/artifactory/api/copy/$SOURCE_REPO/$IMAGE_NAME/$TAG?to=/$TARGET_REPO/$IMAGE_NAME/$TAG"
+RELEASEBUNDLE_PROMOTION_API_URL="$JFROG_URL/lifecycle/api/v2/promotion/records/$RELEASEBUNDLE_NAME/$RELEASEBUNDLE_VERSION"
 
 # Send the promotion request and capture the response as well as the HTTP status code
-RESPONSE=$(curl -s -X POST "$DOCKER_COPY_API_URL" \
+RESPONSE=$(curl -s -X POST "$RELEASEBUNDLE_PROMOTION_API_URL" \
 -H "Content-Type: application/json" \
+-d "{ "environment": $ENVIRONMENT,"included_repository_keys": [],"excluded_repository_keys": [] }"
 -H "Authorization: Bearer $ACCESS_TOKEN")
 
 # Check for success or failure in the response
